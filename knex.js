20f5cca96f88773c0323827fed0b97e0912b3b5f
Aden Collinge <aden.collinge@gmail.com>
2018-05-01 22:37:09 +0000

    milestones !

--- knex.js
+++ knex.js
@@ -15,6 +15,6 @@ knex('famous_people')
   .insert({first_name: process.argv[2], last_name: process.argv[3], birthdate: process.argv[4]})
   .then(() => console.log("done"))
   .catch(err => console.log("error: ", err))
-  .finally(()=> knex.destroy());
+  .finally()=> knex.destroy());
 
 
